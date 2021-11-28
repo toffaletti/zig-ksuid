205ff0df2b3ad4460fbf57c2e3f8f8696efc8c51
Jason Toffaletti <jason@m1max.localdomain>
2021-11-28 13:30:04 -0800

    add test coverage

--- build.zig
+++ build.zig
@@ -15,8 +15,20 @@ pub fn build(b: *std.build.Builder) void {
     lib.setBuildMode(mode);
     lib.install();
 
+    const coverage = b.option(bool, "coverage", "Generate test coverage") orelse false;
+
     var main_tests = b.addTest("src/ksuid.zig");
     main_tests.setBuildMode(mode);
+
+    if (coverage) {
+        main_tests.setExecCmd(&[_]?[]const u8{
+            "kcov",
+            "--clean",
+            "--include-path=./src/",
+            "kcov-output",
+            null, // to get zig to use the --test-cmd-bin flag
+        });
+    }
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
